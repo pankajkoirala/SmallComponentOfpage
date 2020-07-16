544278ebda97ee4c4d212eead4014f9d0b9794df
pankajkoirala <pankaj.koirala.77@gmail.com>
2020-07-16 15:54:41 +0545

    component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Cheese from "./component/cheseContainer"
+import MoreLessButton from  "../src/component/lessAndmore/morelessButton"
 import './App.css';
 
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
   
-  <Cheese/>
+ <MoreLessButton/>
     </div>
   );
 }
---
+++ src/component/lessAndmore/morelessButton.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import a from "../asets/a.jpg";
+import "./readLessMore.css"
+
+import jsonData from "./moreless.json";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardSubtitle,
+  Button,
+} from "reactstrap";
+
+let MoreLessButton = () => {
+  const [data, setData] = useState([]);
+  const [selectedId, setSelectesId] = useState([]);
+  console.log(data);
+  console.log(selectedId);
+
+  useEffect(() => {
+    setData(jsonData);
+  }, []);
+
+  let addId = (id) => {
+    setSelectesId([...selectedId, id]);
+  };
+  let removeId = (id) => {
+    setSelectesId(selectedId.filter((arg) => arg !== id));
+  };
+  return (
+    <div className=" row h-100 w-100 ">
+      {data.map((arg) => {
+        return (
+          <div className="col-4">
+            <div>
+              <Card>
+                <CardImg top width="100%" src={a} alt="Card image cap" />
+                {selectedId.includes(arg.id)}
+
+                <CardBody>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText
+                    className={selectedId.includes(arg.id) ? "" : "less"}
+                  >
+                    {arg.detail}
+                  </CardText>
+                  {selectedId.includes(arg.id) ?  (
+                    <Button
+                      onClick={() => {
+                        removeId(arg.id);
+                      }}
+                      className="m-2"
+                    >
+                      {" "}
+                      read less
+                    </Button>
+                  ):(
+                    <Button
+                      onClick={() => {
+                        addId(arg.id);
+                      }}
+                      className="m-2"
+                    >
+                      read more
+                    </Button>
+                  ) }
+                </CardBody>
+              </Card>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default MoreLessButton;
