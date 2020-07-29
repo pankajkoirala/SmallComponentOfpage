8609b1f56fc55918e04271728c6ed1d9984d835a
pankajkoirala <pankaj.koirala.77@gmail.com>
2020-07-29 15:19:33 +0545

    map

---
+++ src/component/inputMap.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+//go to map and give location and click on share and go to embed a map copy html and insert in component
+
+const InsertMap=()=>{
+  return(
+    <div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.781863301913!2d85.31165421512814!3d27.693136132723115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19b2acf257d5%3A0xbd945210d4a1c500!2sNepal%20Eye%20Hospital!5e0!3m2!1sen!2snp!4v1596014926623!5m2!1sen!2snp" width="800" height="600" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+  )
+}
+
+export default InsertMap
+
+
+
