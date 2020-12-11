b6b9bc36aa48e7e1a160225d45401c00e0383304
pankajkoirala <pankaj.koirala.77@gmail.com>
2020-12-11 20:15:18 +0545

    webpade small component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import MoreNlessContainer from "../src/container/lessNmore";
-import OtherFileContainer from "./component/axiosConfig/otherFileContainer";
 import "./App.css";
-import ImageEntryAsBackground from "../src/component/imageAsBackground/imageBackground";
-import Nav from "../src/component/rough/roug";
-import NoticeModule from "./component/Notice_popup_module/module";
-import Test1 from "../src/component/routerTest/test1";
-import Test2 from "../src/component/routerTest/test2";
-import Test3 from "../src/component/routerTest/test3";
-import Index from "./component/privateRouter/index";
-import HidenNavBar from "./component/NavbarType/hiddenWlileScroleNavBar/navbar"
-import NestedRouter from "./component/nestedRouter/nestedRouter"
-import AutoSearch from "../src/component/autosearch"
+
+import BurgerComponent from "../src/component/burger/burgerContainer";
 
 function App() {
   return (
     <div className="App">
-     
- 
-       <AutoSearch/>
-        
-     
+      <BurgerComponent />
     </div>
   );
 }
 
 export default App;
-//<BurgerComponent/>
+//import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import MoreNlessContainer from "../src/container/lessNmore";
+// import OtherFileContainer from "./component/axiosConfig/otherFileContainer";
+// import ImageEntryAsBackground from "../src/component/imageAsBackground/imageBackground";
+// import Nav from "../src/component/rough/roug";
+// import NoticeModule from "./component/Notice_popup_module/module";
+// import Test1 from "../src/component/routerTest/test1";
+// import Test2 from "../src/component/routerTest/test2";
+// import Test3 from "../src/component/routerTest/test3";
+// import Index from "./component/privateRouter/index";
+// import HidenNavBar from "./component/NavbarType/hiddenWlileScroleNavBar/navbar";
+// import NestedRouter from "./component/nestedRouter/nestedRouter";
+// //import AutoSearch from "../src/component/autosearch";
 
-{
-  /* <OtherFileContainer/>
+/* <OtherFileContainer/>
      <Index />
           <NestedRouter/>
        <HidenNavBar/>
 <NoticeModule />
 <Nav />
+<AutoSearch />
 <Switch>
   <Route exact path={`/test1`} component={Test1} />
   <Route exact path={`/test2`} component={Test2} />
   <Route exact path={`/profile`} component={Test3} />
 </Switch> */
-}
